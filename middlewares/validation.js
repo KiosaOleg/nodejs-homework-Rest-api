50bf348c788a98514b66f81c9d38164fb5866f91
KiosaOleg <117593116+KiosaOleg@users.noreply.github.com>
2023-08-27 23:40:08 +0200

    hw2

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,12 +1,29 @@
-const validation = (schema) => {
+// const validation = (schema) => {
+//   return (req, res, next) => {
+//     const { error } = schema.validate(req.body);
+//     if (error) {
+//       error.status = 400;
+//       next(error);
+//     }
+//     next();
+//   };
+// };
+
+// module.exports = validation;
+
+function validation(schema) {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
+
     if (error) {
       error.status = 400;
-      next(error);
+      return res
+        .status(400)
+        .json({ error: error.details.map((err) => err.message).join(", ") });
     }
+
     next();
   };
-};
+}
 
 module.exports = validation;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,99 +2,30 @@ const express = require("express");
 
 const router = express.Router();
 
-const validation = require("../../middlewares/validation");
-const contactsSchema = require("../../schemas/index");
+const contactsController = require("../../controlers/contactsController");
+
+const { validation, ctrlWrapper } = require("../../middlewares");
+
+const { contactsSchema } = require("../../schemas");
 
 const validateMiddleware = validation(contactsSchema);
 
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../../models/contacts");
+router.get("/", ctrlWrapper(contactsController.getContacts));
 
-// router.get("/", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+router.get("/:contactId", ctrlWrapper(contactsController.getContact));
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
-    res.status(200).json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
+router.post(
+  "/",
+  validateMiddleware,
+  ctrlWrapper(contactsController.createContact)
+);
 
-// router.get("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+router.delete("/:contactId", ctrlWrapper(contactsController.deleteContact));
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const contactId = req.params.contactId;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).json({ message: "Contact not found" });
-      return;
-    }
-    res.status(200).json(contact);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// router.post("/", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
-
-router.post("/", validateMiddleware, async (req, res, next) => {
-  try {
-    const newContact = req.body;
-    const addedContact = await addContact(newContact);
-    res.status(201).json(contact);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// router.delete("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const contactId = req.params.contactId;
-    const removedContact = await removeContact(contactId);
-    if (!removedContact) {
-      res.status(404).json({ message: "Contact not found" });
-      return;
-    }
-    res.status(200).json({ message: "Contact deleted" });
-  } catch (error) {
-    next(error);
-  }
-});
-
-// router.put("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
-
-router.put("/:contactId", validateMiddleware, async (req, res, next) => {
-  try {
-    const contactId = req.params.contactId;
-    const updatedFields = req.body;
-    const updatedContact = await updateContact(contactId, updatedFields);
-    if (!updatedContact) {
-      res.status(404).json({ message: "Contact not found" });
-      return;
-    }
-    res.status(200).json(updatedContact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put(
+  "/:contactId",
+  validateMiddleware,
+  ctrlWrapper(contactsController.updateExistingContact)
+);
 
 module.exports = router;
