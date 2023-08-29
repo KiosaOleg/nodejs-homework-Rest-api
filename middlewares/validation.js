561fa55e30c0cf80681aff125fa3fd156ea0410f
KiosaOleg <117593116+KiosaOleg@users.noreply.github.com>
2023-08-29 21:58:21 +0200

    gf
    fg

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,18 +1,9 @@
-// const validation = (schema) => {
-//   return (req, res, next) => {
-//     const { error } = schema.validate(req.body);
-//     if (error) {
-//       error.status = 400;
-//       next(error);
-//     }
-//     next();
-//   };
-// };
-
-// module.exports = validation;
-
 function validation(schema) {
   return (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({ message: "missing fields" });
+    }
+
     const { error } = schema.validate(req.body);
 
     if (error) {
